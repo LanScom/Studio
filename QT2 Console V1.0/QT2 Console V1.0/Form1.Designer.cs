23b8c174fe3ee6f1d73966a8f3c2a9fa378d4db9
LanScom <930371781@qq.com>
2016-04-19 00:54:32 +0800

    优化串口功能
    1.添加波特率和串口号
    2.添加测试项命令

---
+++ QT2 Console V1.0/QT2 Console V1.0/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace QT2_Console_V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.w_serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // w_serialPort
+            // 
+            this.w_serialPort.BaudRate = 115200;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(121, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Test Pass";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 60);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(121, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Test Fail";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(529, 140);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Summary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Invertal(S)";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(83, 101);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(42, 21);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Received";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(347, 36);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 21);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Code VER.";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(347, 63);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 50);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "Code VER.\r\n\r\nCode VER.=1.0 \r\n@_@";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Send";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 167);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(529, 273);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Failure Record";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 24);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(499, 235);
+            this.textBox3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 452);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WORLD Serial Board Reliability Testing GUI V1.1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort w_serialPort;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ QT2 Console V1.0/QT2 Console V1.0/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Text;
+using System.IO;
+
+namespace QT2_Console_V1._0
+{
+    public partial class Form1 : Form
+    {
+        //Thread _readThread;
+        //bool _keepReading;
+        #region Temp Data
+        long intcount_pass = 0;
+        long intcount_fail = 0;
+        int timeout_cnt = 0;
+        bool button1_status = false;
+        string send_mcu_str = "";
+        string receive_mcu_str = "";
+        #endregion
+        string tmp_str = "";
+        string tmp_com = "";
+        public Form1()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();         // get the active serial port name;
+            foreach (string port in ports)
+            {
+                tmp_com += port;
+
+            }
+
+            if (tmp_com == "")
+            {
+                MessageBox.Show("Could not detect serial port!");
+            }
+            else
+            {
+                w_serialPort.PortName = tmp_com;
+                w_serialPort.BaudRate = 115200;
+                w_serialPort.StopBits = StopBits.One;
+                w_serialPort.WriteTimeout = 3000;
+                w_serialPort.ReadTimeout = 3000;
+                w_serialPort.ReceivedBytesThreshold = 1;
+                w_serialPort.DataReceived += new SerialDataReceivedEventHandler(com_DataReceive);
+                w_serialPort.Open();
+                //_keepReading = true;
+                //_readThread = new Thread(ReadPort);
+                // _readThread.Start();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tmp_com == "")
+            {
+                MessageBox.Show("Please Open the Serial Port Firstly!");
+            }
+            else
+            {
+                if (button1_status == false)
+                {
+                    button1_status = true;
+                    button1.Text = "Stop";
+                    send_mcu_str = textBox4.Text;
+                    receive_mcu_str = "Code VER.\n\r\nCode VER.=1.0 \r\n@_@";
+                    //receive_mcu_str = "Code VER.\n\r\nCode VER.=1.0 \r\n@_@";
+
+                    w_serialPort.WriteLine(send_mcu_str);
+                    timer1.Interval = (int)(double.Parse(textBox6.Text) * 1000);
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    button1_status = false;
+                    timer1.Enabled = false;
+                    button1.Text = "Start";
+                }
+            }
+        }
+
+        //private void ReadPort()
+        //{
+        //    while(_keepReading)
+        //    {
+        //        if(w_serialPort.IsOpen)
+        //        {
+        //            byte[] readBuffer = new byte[w_serialPort.ReadBufferSize + 1];
+        //            try
+        //            {
+        //                int count = w_serialPort.Read(readBuffer, 0, w_serialPort.ReadBufferSize);
+        //                string serialIn = System.Text.Encoding.ASCII.GetString(readBuffer, 0, count);
+        //                if(count != 0)
+        //                {
+        //                    //threadfun
+        //                    //timer1.Enabled = false;
+        //                  // textBox1.Text += serialIn;
+        //                }
+        //            }
+        //            catch(TimeoutException)
+        //            {
+        //                timer1.Enabled = false;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            TimeSpan waittime = new TimeSpan(0, 0, 0, 0, 50);
+        //            Thread.Sleep(waittime);
+        //        }
+        //    }
+        //}
+
+        private void com_DataReceive(object sender,SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(30);
+            int count = w_serialPort.BytesToRead;
+            if (count <= 0)
+                return;
+            byte[] readBuffer = new byte[count];
+            byte[] readBuffer1 = new byte[count+1];
+            Thread.Sleep(10);
+            w_serialPort.Read(readBuffer, 0, count);
+            string str = Encoding.ASCII.GetString(readBuffer);
+            tmp_str = str;
+            //textBox2.Text += str;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            if(tmp_str == receive_mcu_str)       //"Code VER.\n\r\nCode VER.=1.0 \r\n@_@"   Code VER.\n\r\nCode VER.=1.0 \r\n@_@   Code VER.\r\n\r\nCode VER.=1.0 \r\n@_@
+            {
+                intcount_pass += 1; 
+                tmp_str = "";                    
+                textBox1.Text = intcount_pass.ToString();
+                Thread.Sleep(2);
+                w_serialPort.WriteLine(send_mcu_str);
+                //Thread.Sleep(10);
+                timer1.Enabled = true;
+            }
+            else if(tmp_str == "")
+            {
+                if (timeout_cnt < 5)
+                {
+                    timeout_cnt++;
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    timeout_cnt = 0;
+                    Thread.Sleep(2);
+                    w_serialPort.WriteLine(send_mcu_str);
+                    //Thread.Sleep(10);
+                    timer1.Enabled = true;
+                }
+
+            }
+            else
+            {
+                intcount_fail += 1;
+                textBox2.Text = intcount_fail.ToString();
+                FileStream fs = new FileStream(@"Failure Summary.csv", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(tmp_str+"\r\n");
+                sw.Close();
+                fs.Close();
+                textBox3.Text += tmp_str+ "\r\n\r\n";
+                tmp_str = "";
+                Thread.Sleep(200);
+                w_serialPort.WriteLine(send_mcu_str);
+                timer1.Enabled = true;
+         
+            }
+        }
+
+    }
+}
